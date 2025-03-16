8169c3b4c8e0cfd4dbab1805ad78f13c2ed7b05c
SCMRCORE <3551147139@qq.com>
2025-03-16 22:32:35 +0800

    init

---
+++ src/main/java/com/example/minischedule/MiniScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.minischedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniScheduleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/minischedule/ScheduleService.java
@@ -0,0 +1,8 @@
+package com.example.minischedule;
+
+public class ScheduleService {
+
+    public void schedule(Runnable task, long delay){
+
+    }
+}
